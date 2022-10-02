334d3ce361bc4a1f7d78f6903e0929726a6c4de2
Julia Maria Vita Pinto <julia.vita@g.globo>
2022-10-01 21:55:47 -0300

    testando

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { connection } = require('./connection');
+const app = express();
+const port = 3000; 
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+
+app.get('/clientes', (req, res) => {
+    connection.query('SELECT * FROM clientes', (error, results, fields) => {
+        if(error) 
+          res.json(error);
+        else
+          res.json(results);
+        connection.end();
+        console.log('executou!');
+    });
+});
+
+app.get('/clientes', (req, res) => {
+  connection.query('SELECT * FROM clientes', (error, results, fields) => {
+      if(error) 
+        res.json(error);
+      else
+        res.json(results);
+      connection.end();
+      console.log('executou!');
+  });
+});
+
+
+app.listen(port);
+console.log('API funcionando!');
